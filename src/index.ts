ed3759273f208610ee1d4547edf18f2c0b8c53d8
Ryan Carniato <ryansolid@gmail.com>
2020-09-24 03:01:48 -0700

    update runtime

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { root, cleanup, lazy, createContext, useContext } from "./lib";
+export { root, cleanup, lazy, createContext, useContext, assignProps, splitProps } from "./lib";
 export type { Context, Component, ComponentProps } from "./lib";
 export * from "./runtime";
 
